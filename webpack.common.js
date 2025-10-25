0b22048d86b2e967be7c18907a780a14ad973638
Bryan Luise Reamillo <137306067+Bleu24@users.noreply.github.com>
2025-10-26 01:55:32 +0800

    Update webpack.common.js
    Add comment to title property under HTMLwebpack plugin

--- webpack.common.js
+++ webpack.common.js
@@ -27,7 +27,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/template.html",
-            title: "webpack-template"
+            title: "webpack-template" // change this, this chanes the title docs
         })
     ]
 }
