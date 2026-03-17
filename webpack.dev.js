26b27a05d1608f991a0e106d7d43e216d7c32106
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-03-17 17:50:57 +0800

    Fix formatting in webpack.dev.js by standardizing quote styles

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,12 @@
-const common = require('./webpack.common.js');
-const { merge } = require('webpack-merge');
+const common = require("./webpack.common.js");
+const { merge } = require("webpack-merge");
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'eval-source-map',
+    mode: "development",
+    devtool: "eval-source-map",
     devServer: {
         port: 5500, // default 8080
-        watchFiles: ['./src/main.js'],
+        watchFiles: ["./src/main.js"],
         open: true
     },
 });
